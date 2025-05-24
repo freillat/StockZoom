e9c03e42fec761ae233fb33f50072edfec6acc84
freillat <freillat@gmail.com>
2025-05-24 21:16:56 +0100

    q3

--- 01-intro/main.py
+++ 01-intro/main.py
@@ -38,4 +38,64 @@ for i in range(0,df_indices['Ticker'].shape[0]):
     ytd_return = ( close - open ) / open
     print(df_indices['Name'][i], "{:.2%}".format(ytd_return))
 
-    # Question 3
+# Question 3
+ticker = '^GSPC'
+ticker_obj = yf.Ticker(ticker)
+start_date='1950-01-01'
+daily = ticker_obj.history(start=start_date)
+
+# rows = daily.shape[0]
+# ath_list = [ 0 ]
+# for i in range(1, rows-1):
+#     if daily['High'].iloc[i]>daily['High'].iloc[ath_list[-1]]:
+#         ath_list.append(i)
+# data = []
+# for i in range(0,len(ath_list)-1):
+#     start=ath_list[i]
+#     end=ath_list[i+1]
+#     low = daily['Low'].iloc[start]
+#     low_idx= 0
+#     for j in range(start, end):
+#         if daily['Low'].iloc[j]<low:
+#             low = daily['Low'].iloc[j]
+#             low_idx=j
+#     high = daily['High'].iloc[ath_list[i]]
+#     drawdown = (high - low) / high
+#     start_date = daily.index[start].date()
+#     end_date = daily.index[end].date()
+#     low_date = daily.index[low_idx].date()
+#     data.append([start, end, low_idx ,low, high, drawdown, start_date, end_date, low_date])
+# df = pd.DataFrame(data)
+# df.columns = [ 'Start', 'End', 'LowIdx', 'Low', 'High', 'Drawdown', 'StartDate', 'EndDate', 'LowDate']
+# df['Duration']=df['LowDate']-df['StartDate']
+# df_filter=df[df['Drawdown']>0.05]
+# print(df_filter['Duration'].quantile(0.5))
+
+rows = daily.shape[0]
+ath_list = [ 0 ]
+for i in range(1, rows-1):
+    if daily['Close'].iloc[i]>daily['Close'].iloc[ath_list[-1]]:
+        ath_list.append(i)
+data = []
+for i in range(0,len(ath_list)-1):
+    start=ath_list[i]
+    end=ath_list[i+1]
+    low = daily['Close'].iloc[start]
+    low_idx= 0
+    for j in range(start, end):
+        if daily['Close'].iloc[j]<low:
+            low = daily['Close'].iloc[j]
+            low_idx=j
+    high = daily['Close'].iloc[ath_list[i]]
+    drawdown = (high - low) / high
+    start_date = daily.index[start].date()
+    end_date = daily.index[end].date()
+    low_date = daily.index[low_idx].date()
+    data.append([start, end, low_idx ,low, high, drawdown, start_date, end_date, low_date])
+df = pd.DataFrame(data)
+df.columns = [ 'Start', 'End', 'LowIdx', 'Low', 'High', 'Drawdown', 'StartDate', 'EndDate', 'LowDate']
+df['Duration']=df['LowDate']-df['StartDate']
+df_filter=df[df['Drawdown']>0.05]
+print(df_filter['Duration'].quantile(0.5))
+
+# Question 4
