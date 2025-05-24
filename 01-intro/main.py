cf104f864880c220d9bb6fec5d1879e41963f47d
freillat <freillat@gmail.com>
2025-05-24 16:24:00 +0100

    q1 updated

--- 01-intro/main.py
+++ 01-intro/main.py
@@ -1,8 +1,7 @@
 import pandas as pd
 
 url_sp500 = "https://en.wikipedia.org/wiki/List_of_S%26P_500_companies"
-df_url_sp500 = pd.read_html(url_sp500)
-df_sp500 = df_url_sp500[0]
+df_sp500 = pd.read_html(url_sp500)[0]
 df_sp500['Year added']=df_sp500['Date added'].str[:4]
-print(df_sp500.head())
 print(df_sp500['Year added'].value_counts()[:5])
+
